bacc9bff84620ef83360f0f689a86e23629f89ba
Alex Manoel <alexmanoel93@gmail.com>
2024-06-18 16:45:03 -0300

    feature(project): First class finished

---
+++ apps/partner2/src/eventos/eventos.controller.ts
@@ -0,0 +1,75 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  UseGuards,
+} from '@nestjs/common';
+import { CriarEventoRequest } from './request/criar-evento.request';
+import { AtualizarEventoRequest } from './request/atualizar-evento.request';
+import { ReservarLugarRequest } from './request/reservar-lugar.request';
+import { EventsService } from '@app/core/events/events.service';
+import { TicketKind } from '@prisma/client';
+import { AuthGuard } from '@app/core/auth/auth.guard';
+
+@Controller('eventos')
+export class EventosController {
+  constructor(private readonly eventsService: EventsService) {}
+
+  @Post()
+  create(@Body() criarEventoRequest: CriarEventoRequest) {
+    return this.eventsService.create({
+      name: criarEventoRequest.nome,
+      description: criarEventoRequest.descricao,
+      date: criarEventoRequest.data,
+      price: criarEventoRequest.preco,
+    });
+  }
+
+  @Get()
+  findAll() {
+    return this.eventsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.eventsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() atualizarEventoRequest: AtualizarEventoRequest,
+  ) {
+    return this.eventsService.update(id, {
+      name: atualizarEventoRequest.nome,
+      description: atualizarEventoRequest.descricao,
+      date: atualizarEventoRequest.data,
+      price: atualizarEventoRequest.preco,
+    });
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.eventsService.remove(id);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post(':id/reservar')
+  reserveSpots(
+    @Body() reservarLugar: ReservarLugarRequest,
+    @Param('id') eventoId: string,
+  ) {
+    return this.eventsService.reserveSpot({
+      eventId: eventoId,
+      spots: reservarLugar.lugares,
+      ticket_kind: reservarLugar.tipo_ingresso === 'inteira' ? TicketKind.full : TicketKind.half,
+      email: reservarLugar.email,
+    });
+  }
+}
